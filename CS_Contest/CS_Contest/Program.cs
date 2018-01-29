519941048116de8c47787c352999cc2dc3532906
xztaityozx <taityonohibi@gmail.com>
2018-01-29 22:05:52 +0900

    abc051_d AC

--- CS_Contest/CS_Contest/Program.cs
+++ CS_Contest/CS_Contest/Program.cs
@@ -6,6 +6,9 @@ using System.Net;
 using static System.Console;
 using static System.Math;
 using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 using static CS_Contest.Utils;
 using static Nakov.IO.Cin;
 
@@ -15,7 +18,8 @@ namespace CS_Contest {
 	using Li = List<int>;
 	using LLi = List<List<int>>;
 	using Ll = List<long>;
-
+	using ti3=Tuple<int,int,int>;
+	using Box=List<List<char>>;
 	internal class Program {
 		private static void Main(string[] args) {
 			var sw = new StreamWriter(OpenStandardOutput()) { AutoFlush = false };
@@ -27,26 +31,185 @@ namespace CS_Contest {
 		public class Calc {
 			public void Solve() {
 				int N = NextInt(), M = NextInt();
-				var imos = new int[M + 1];
-				int sum = 0;
-				REP(N, _ => {
-					int li = NextInt()-1, ri = NextInt()-1, si = NextInt();
-					sum += si;
-					imos[li] += si;
-					imos[ri] -= si;
+				var wf = new WarshallFloyd(N);
+				var list = new List<ti3>();
+				REP(M, _ =>
+				{
+					int ai = NextInt(), bi = NextInt(), ci = NextInt();
+					ai--;
+					bi--;
+					wf.Add(ai, bi, ci, false);
+					list.Add(new ti3(ai, bi, ci));
+				});
+				var box = wf.Run();
+
+				var ans = M;
+
+				REP(M, i =>
+				{
+					bool f = false;
+					REP(N, k =>
+					{
+						if (box[k][list[i].Item1] + list[i].Item3 == box[k][list[i].Item2]) f = true;
+					});
+					if (f) ans--;
 				});
 
-				REP(M, _ => {
-					imos[_ + 1] += imos[_];
-				});
-
-				var min = imos.Take(M-1).Min();
-				(sum-min).WL();
-
+				ans.WL();
 				return;
 			}
+		}
+	}
+
+	/// <summary>
+	/// 優先度付きキュー
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class PriorityQueue<T> {
+		private readonly List<T> heap;
+		private readonly Comparison<T> compare;
+		private int size;
+
+		public PriorityQueue() : this(Comparer<T>.Default) {
+		}
+
+		public PriorityQueue(IComparer<T> comparer) : this(16, comparer.Compare) {
+		}
+
+		public PriorityQueue(Comparison<T> comparison) : this(16, comparison) {
+		}
+
+		public PriorityQueue(int capacity, Comparison<T> comparison) {
+			this.heap = new List<T>(capacity);
+			this.compare = comparison;
+		}
+
+		public void Enqueue(T item) {
+			this.heap.Add(item);
+			var i = size++;
+			while (i > 0) {
+				var p = (i - 1) >> 1;
+				if (compare(this.heap[p], item) <= 0)
+					break;
+				this.heap[i] = heap[p];
+				i = p;
+			}
+			this.heap[i] = item;
+		}
+
+		public T Dequeue() {
+			var ret = this.heap[0];
+			var x = this.heap[--size];
+			var i = 0;
+			while ((i << 1) + 1 < size) {
+				var a = (i << 1) + 1;
+				var b = (i << 1) + 2;
+				if (b < size && compare(heap[b], heap[a]) < 0) a = b;
+				if (compare(heap[a], x) >= 0)
+					break;
+				heap[i] = heap[a];
+				i = a;
+			}
+			heap[i] = x;
+			heap.RemoveAt(size);
+			return ret;
+		}
+
+		public T Peek() {
+			return heap[0];
+		}
+
+		public int Count => size;
+
+		public bool Any() {
+			return size > 0;
+		}
+	}
+
+	public class CostGraph
+	{
+		public struct Edge 
+		{
+			public int To { get; set; }
+			public long Cost { get; set; }
 
 
+			public Edge(int to, long cost) {
+				To = to;
+				Cost = cost;
+			}
+
+		}
+
+		public int Size { get; set; }
+		public List<List<Edge>> Adjacency { get; set; }
+		public const long Inf = (long) 1e15;
+
+		public CostGraph(int size) {
+			Size = size;
+			Adjacency = new List<List<Edge>>();
+			REP(Size, _ => Adjacency.Add(new List<Edge>()));
+		}
+
+		public void Add(int s, int t, long c, bool dir = true) {
+			Adjacency[s].Add(new Edge(t, c));
+			if (!dir) Adjacency[t].Add(new Edge(s, c));
+		}
+	}
+
+	public class Dijkstra : CostGraph
+	{
+		public Dijkstra(int size) : base(size) {}
+		public int[] PreviousNodeList { get; set; }
+		public long[] Distance { get; set; }
+
+		public void Run(int s) {
+			PreviousNodeList=new int[Size];
+			Distance = new long[Size];
+			REP(Size, _ => Distance[_] = Inf);
+
+			var pq = new PriorityQueue<Edge>((x, y) => x.Cost.CompareTo(y.Cost));
+			Distance[s] = 0;
+			pq.Enqueue(new Edge(s, 0));
+			while (pq.Any()) {
+				var src = pq.Dequeue();
+				if(Distance[src.To]<src.Cost) continue;
+				for (var i = 0; i < Adjacency[src.To].Count; i++) {
+					var tmp = Adjacency[src.To][i];
+					var cost = tmp.Cost + src.Cost;
+					if(cost>=Distance[tmp.To]) continue;
+					Distance[tmp.To] = cost;
+					pq.Enqueue(new Edge(tmp.To,cost));
+					PreviousNodeList[tmp.To] = src.To;
+				}
+			}
+		}
+	}
+
+	public class WarshallFloyd : CostGraph
+	{
+		public WarshallFloyd(int size) : base(size) {
+		}
+
+		public List<Ll> Run() {
+			var rt = new List<Ll>();
+			REP(Size, _ => rt.Add(new Ll()));
+
+			REP(Size, i => REP(Size, k => rt[i].Add( i == k ? 0 : Inf)));
+
+			ForeachWith(Adjacency, (i, item) =>
+			{
+				foreach (var k in item) {
+					rt[i][k.To] = k.Cost;
+				}
+			});
+
+			REP(Size, i => REP(Size, j => REP(Size, k =>
+			{
+				rt[j][k] = Min(rt[j][k], rt[j][i] + rt[i][k]);
+			})));
+
+			return rt;
 		}
 	}
 
@@ -57,6 +220,7 @@ namespace CS_Contest {
 
 		public static void WL(this string obj) => WriteLine(obj);
 
+
 		public static void WL<T>(this IEnumerable<T> list) => list.ToList().ForEach(x => x.WL());
 
 		public static Li ReadInts() => ReadLine().Split().Select(int.Parse).ToList();
@@ -66,6 +230,14 @@ namespace CS_Contest {
 		public static void REP(int n, Action<int> act) {
 			for (var i = 0; i < n; i++) {
 				act(i);
+			}
+		}
+
+		public static void ForeachWith<T>(IEnumerable<T> ie, Action<int, T> act) {
+			var i = 0;
+			foreach (var item in ie) {
+				act(i, item);
+				i++;
 			}
 		}
 
