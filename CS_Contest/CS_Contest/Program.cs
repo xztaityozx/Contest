70b7c38c467a6fe67e2bea47f2f793d383d086f9
xztaityozx <taityonohibi@gmail.com>
2018-03-05 16:49:50 +0900

    ARC074 D AC

--- CS_Contest/CS_Contest/Program.cs
+++ CS_Contest/CS_Contest/Program.cs
@@ -30,12 +30,113 @@ namespace CS_Contest {
 		public class Calc
 		{
 			public void Solve() {
-				long X = NextLong(), Y = NextLong();
-				
-				if(Abs(X-Y)<=1) "Brown".WL();
-				else "Alice".WL();
+				int N = NextInt();
+				var A = NextLongList();
+
+				var pq=new PriorityQueue<long>();
+
+				var pqb = new PriorityQueue<long>();
+
+				long bsum = 0L;
+				long asum = A.Take(N).Sum();
+
+				for (int i = 0; i < N; i++) {
+					pq.Enqueue(A[i]);
+				}
+
+				for (int i = 3 * N - 1; i >= 2 * N; i--) {
+					pqb.Enqueue(-A[i]);
+					bsum += A[i];
+				}
+
+				var max = new long[3 * N];
+				max[N - 1] = asum;
+				var min = new long[3 * N];
+				min[2 * N] = bsum;
+
+				for (int k = N; k <= 2*N; k++) {
+					pq.Enqueue(A[k]);
+					var src = pq.Dequeue();
+					asum = asum - src + A[k];
+					max[k] = asum;
+				}
+
+
+				for (int k = 2 * N - 1; k >= N; k--) {
+					bsum += A[k];
+					pqb.Enqueue(-A[k]);
+					var m = pqb.Dequeue();
+					bsum -= -m;
+					min[k] = bsum;
+				}
+
+				var ans = long.MinValue;
+				for (int k = N-1; k < 2*N; k++) {
+					ans = Max(ans, max[k] - min[k + 1]);
+				}
+				ans.WL();
 
 				return;
+			}
+		}
+		public class PriorityQueue<T> {
+			private readonly List<T> heap;
+			private readonly Comparison<T> compare;
+			private int size;
+
+			public PriorityQueue() : this(Comparer<T>.Default) {
+			}
+
+			public PriorityQueue(IComparer<T> comparer) : this(16, comparer.Compare) {
+			}
+
+			public PriorityQueue(Comparison<T> comparison) : this(16, comparison) {
+			}
+
+			public PriorityQueue(int capacity, Comparison<T> comparison) {
+				this.heap = new List<T>(capacity);
+				this.compare = comparison;
+			}
+
+			public void Enqueue(T item) {
+				this.heap.Add(item);
+				var i = size++;
+				while (i > 0) {
+					var p = (i - 1) >> 1;
+					if (compare(this.heap[p], item) <= 0)
+						break;
+					this.heap[i] = heap[p];
+					i = p;
+				}
+				this.heap[i] = item;
+			}
+
+			public T Dequeue() {
+				var ret = this.heap[0];
+				var x = this.heap[--size];
+				var i = 0;
+				while ((i << 1) + 1 < size) {
+					var a = (i << 1) + 1;
+					var b = (i << 1) + 2;
+					if (b < size && compare(heap[b], heap[a]) < 0) a = b;
+					if (compare(heap[a], x) >= 0)
+						break;
+					heap[i] = heap[a];
+					i = a;
+				}
+				heap[i] = x;
+				heap.RemoveAt(size);
+				return ret;
+			}
+
+			public T Peek() {
+				return heap[0];
+			}
+
+			public int Count => size;
+
+			public bool Any() {
+				return size > 0;
 			}
 		}
 
