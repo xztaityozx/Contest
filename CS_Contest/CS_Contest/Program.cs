18784af0e6ba47c244c5e6cce02e873d574e752d
xztaityozx <taityonohibi@gmail.com>
2018-03-17 23:04:42 +0900

    ARC092 C AC

--- CS_Contest/CS_Contest/Program.cs
+++ CS_Contest/CS_Contest/Program.cs
@@ -31,44 +31,117 @@ namespace CS_Contest {
 		public class Calc
 		{
 			public void Solve() {
-				int N = NextInt(), X = NextInt();
-				var dp = new long[N][];
-				N.REP(i => dp[i] = new long[N + 1]);
-				dp[0][1] = X;
-				var T = NextIntList(N);
+				int N = NextInt();
+				var Alist = new List<ti3>();
+				var Blist = new List<ti3>();
 
-				var n = 0;
+				var s = 0;
+				var t = 2 * N + 1;
+
+				var mf = new MaxFlow(t+1);
 
 				N.REP(i =>
 				{
-					while (n < N && T[n] - T[i] < X) n++;
+					var ai = NextInt();
+					var bi = NextInt();
 
-					for (int j = 0; j+1 <= N; j++) {
-						var v = dp[i][j];
-						if (i + 1 < N) {
-							dp[i + 1][j + 1] = Max(dp[i + 1][j + 1], v + Min(X, T[i + 1] - T[i]));
-						}
+					Alist.Add(new ti3(ai, bi, i + 1));
+					mf.Add(s, i + 1, 1);
+				});
+				
+				N.REP(i =>
+				{
+					var ai = NextInt();
+					var bi = NextInt();
 
-						if (n - 1 > i) {
-							dp[n-1][j + 1] = Max(dp[n-1][j + 1], v + Min(X, T[n - 1] - T[i]));
-						}
-
-						if (n > i && n < N) {
-							dp[n][j + 1] = Max(dp[n][j + 1], v + Min(X, T[n] - T[i]));
-						}
-					}
+					Alist.Add(new ti3(ai, bi, i + N+1));
+					mf.Add(i + N + 1, t, 1);
 				});
 
-				for (int i = 1; i <= N; i++) {
-					var ans = 0L;
-					N.REP(j => ans = Max(ans, dp[j][i]));
-					ans.WL();
+				var list = Alist.Concat(Blist).OrderBy(x => x.Item1).Select(item => new ti2(item.Item2, item.Item3)).ToList();
+
+				for (int i = 0; i < list.Count; i++) {
+					var a = list[i].Item2;
+					if (a > N) continue;
+					var ay = list[i].Item1;
+					for (int j = i+1; j < list.Count; j++) {
+						var b = list[j].Item2;
+						if (b < N + 1) continue;
+						var by = list[j].Item1;
+						if(by<ay) continue;
+
+						mf.Add(a,b,1);
+
+					}
 				}
+
+				mf.Run(s,t).WL();
 
 				return;
 			}
 		}
 
+		public class MaxFlow {
+			private class Edge {
+				public int To, Reverse, Capacity;
+			}
+
+			private int V { get; set; }
+			private List<Edge>[] graph { get; set; }
+			private int[] leveList, itr;
+			public MaxFlow(int v) {
+				V = v;
+				graph = Enumerable.Repeat(1, V).Select(x => new List<Edge>()).ToArray();
+			}
+
+			public void Add(int from, int to, int capa, bool dir = true) {
+				graph[from].Add(new Edge { Capacity = capa, Reverse = graph[to].Count, To = to });
+				graph[to].Add(new Edge { To = from, Capacity = dir ? 0 : capa, Reverse = graph[from].Count - 1 });
+			}
+
+			private void Bfs(int s) {
+				leveList = Enumerable.Repeat(-1, V).ToArray();
+				var queue = new Queue<int>();
+				leveList[s] = 0;
+				queue.Enqueue(s);
+				while (queue.Any()) {
+					var src = queue.Dequeue();
+					foreach (var edge in graph[src]) {
+						if (edge.Capacity <= 0 || leveList[edge.To] >= 0) continue;
+						leveList[edge.To] = leveList[src] + 1;
+						queue.Enqueue(edge.To);
+					}
+				}
+			}
+
+			private int Dfs(int v, int t, int f) {
+				if (v == t) return f;
+				for (; itr[v] < graph[v].Count; itr[v]++) {
+					var edge = graph[v][itr[v]];
+					if (edge.Capacity <= 0 || leveList[v] >= leveList[edge.To]) continue;
+					var d = Dfs(edge.To, t, Min(f, edge.Capacity));
+					if (d <= 0) continue;
+					edge.Capacity -= d;
+					graph[edge.To][edge.Reverse].Capacity += d;
+					return d;
+				}
+
+				return 0;
+			}
+
+			public int Run(int s, int t) {
+				int rt = 0;
+				Bfs(s);
+				while (leveList[t] >= 0) {
+					itr = new int[V];
+					int f;
+					while ((f = Dfs(s, t, int.MaxValue)) > 0) rt += f;
+					Bfs(s);
+				}
+
+				return rt;
+			}
+		}
 
 	}
 }
