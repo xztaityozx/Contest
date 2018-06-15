f18f868644383a90f9521108787ef9eb6f7a1e6e
xztaityozx <taityonohibi@gmail.com>
2018-06-16 02:47:13 +0900

    AOJ Vampire AC

--- cpp/contest.cpp
+++ cpp/contest.cpp
@@ -1,87 +1,49 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-#include<functional>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-#define rep(i,n) for(int i=0;i<n;i++)
+#define rep(i,n) for(int i=0;i<(n);++i)
 #define out(S) cout<<(S)<<endl;
 #define ShowAll(collection) for(auto i:collection){out(i);}
-
+#define beginend(v) v.begin(),v.end()
 
 using pii=pair<int,int>;
 using ll=long long;
 template<typename T> void removeAt(vector<T>& v, int index) { v.erase(v.begin() + index); }
 
-pair<pii,pii> cut(const pii& cake, int si){
 
+bool box[21][41];
+void solve(int r,int n){
+  rep(y,21) rep(x,41) box[y][x]=false;
+  rep(i,n) {
+    int li,ri,hi;
+    cin >> li >> ri >> hi;
 
-  auto w=cake.first;
-  auto d=cake.second;
-  auto r=w*2+d*2;
-  si%=r;
-  //cout << w << " " << d << endl;
-  if(si<=w){
-    return pair<pii,pii>(
-            pii(min(si,w-si),d),
-            pii(max(si,w-si),d)
-          );
-  }else if(si<=w+d){
-    return pair<pii,pii>(
-          pii(w,min(si-w,d-(si-w))),
-          pii(w,max(si-w,d-(si-w)))
-        );
-  }else if(si<=w*2+d){
-    return pair<pii,pii>(
-          pii(min(si-d-w,w-(si-d-w)),d),
-          pii(max(si-d-w,w-(si-d-w)),d)
-        );
+    for(int y=0;y<hi;++y) {
+      for(int x=li+20;x<ri+20;++x){
+        box[y][x]=true;
+      }
+    }
   }
-  return pair<pii,pii>(
-          pii(w,min(si-w-w-d,d-(si-w-w-d))),
-          pii(w,max(si-w-w-d,d-(si-w-w-d)))
-        );
-}
-
-void solve(int n, int w, int d){
-  vector<pii> box;
-  box.push_back(pii(w,d));
-
-  rep(i,n){
-    int pi,si;
-    cin >> pi>>si;
-    auto res = cut(box[pi-1],si);
-    box.push_back(res.first);
-    box.push_back(res.second);
-    removeAt(box,pi-1);
-    //cout << "end step" << endl;
-    //for(auto item : box) {
-      //cout << item.first << " " << item.second << endl;
-    //}
+  auto ans=numeric_limits<double>::max();
+  rep(y,21){
+    for(int x=-r+20;x<r+20;++x) {
+      if(!box[y][x]){
+        double time;
+        if(x<20) time = (double) y+r-sqrt(pow((double)r,2.0)-pow(x-19.0, 2.0));
+        else time = (double) y+r-sqrt(pow((double)r,2.0)-pow(x-20.0, 2.0));
+        ans=min(ans,time);
+      }
+    }
   }
-  
-  vector<ll> ans;
-  for(auto item : box){
-    ans.push_back(item.first*item.second);
-  }
-  sort(ans.begin(),ans.end());
-
-  auto len=ans.size();
-  for(int i=0;i<len-1;i++){
-    cout << ans[i] << " ";
-  }
-  cout << ans[len-1] << endl;
+  printf("%.4lf\n",ans);
 }
 
 int main()
 {
-  int n,w,d;
-  cin >> n>>w>>d;
-  while(n||w||d){
-    solve(n,w,d);
-    cin >> n>>w>>d;
+  int r,n;
+  while(cin>>r>>n && r){
+    solve(r,n);
   }
   return 0;
 }
