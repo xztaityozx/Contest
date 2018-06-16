42fff57fa020d8969e17020def530c6c990089d2
xztaityozx <taityonohibi@gmail.com>
2018-06-16 14:35:28 +0900

    AOJ 2011 Gather the map

--- cpp/contest.cpp
+++ cpp/contest.cpp
@@ -9,71 +9,47 @@ using namespace std;
 
 using pii=pair<int,int>;
 using ll=long long;
+using ull=unsigned long long;
+using vi=vector<int>;
 template<typename T> void removeAt(vector<T>& v, int index) { v.erase(v.begin() + index); }
 
-struct node {
-    vector<node*> next;
-    ll cost;
-    node* before;
-    node(ll c) : cost(c){};
-    node():cost(0){};
-};
-
-node* make(string s){
-    node* root = new node();
-    auto pos=root;
-    int c=0,n=s.size();
-    while (1){
-        if(c>=n) break;
-        if(s[c]=='['){
-            node* tmp=new node();
-            tmp->before=pos;
-            pos->next.push_back(tmp);
-            pos=tmp;
-            c++;
-        }else if(s[c]==']'){
-            pos=pos->before;
-            c++;
-        }else{
-            int i=c;
-            int num=0;
-            while(1) {
-                if(!isdigit(s[i])) break; else num=num*10+(s[i]-'0');
-                i++;
-            }
-            c=i;
-            pos->cost=(num+1)/2;
-        }
+void solve(int n){
+  bool box[n][31]{{false}};
+  rep(i,n) {
+    int f;
+    cin >> f;
+    rep(k,f) {
+      int x;
+      cin >> x;
+      box[i][x]=true;
     }
-    return root;
-}
+  }
+  ull dp[n];
+  rep(i,n) dp[i] = 1ULL << i;
 
-ll dfs(node* now){
-    if(now->cost!=0) return now->cost;
-    int n=(now->next).size();
-    vector<ll> sum;
-    rep(i,n){
-        sum.push_back(dfs(now->next[i]));
+  for(int i=1;i<=30;++i){
+    vector<bool> get(n,false);
+    ull sub = 0ULL;
+    rep(k,n){
+      for(int m=k+1;m<n;++m) if(box[k][i]&&box[m][i]){
+        get[k]=get[m]=true;
+        sub|=dp[k]|dp[m];
+      }
     }
-    sort(beginend(sum));
-    ll rt=0;
-    for (int j = 0; j < (n + 1) / 2; ++j) {
-        rt+=sum[j];
+    if(sub==(1ULL<<n)-1){
+      cout << i<< endl;
+      return;
     }
-    return rt;
-}
-
-int solve(){
-  string s;
-  cin >> s;
-  auto root=make(s);
-  cout<<dfs(root)<<endl;
+    rep(i,n) if(get[i]) dp[i]=sub;
+  }
+  cout << -1 << endl;
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    rep(i,n) solve();
+  int n;
+  while(cin>>n,n){
+    solve(n);
+  }
   return 0;
 }
