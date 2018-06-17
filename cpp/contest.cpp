0f68cd24680b3218e2f9b4a1be3d39452bdcced9
xztaityozx <taityonohibi@gmail.com>
2018-06-17 23:33:35 +0900

    AOJ 2707 Jail

--- cpp/contest.cpp
+++ cpp/contest.cpp
@@ -16,58 +16,20 @@ using vvc=vector<vector<char>>;
 using ti3=tuple<int,int,int>;
 template<typename T> void removeAt(vector<T>& v, int index) { v.erase(v.begin() + index); }
 
-// x=>ABC
-string encode(const string s,const char x){
-  string rt="";
-  for(auto c:s){
-    if(c==x) rt+="ABC";
-    else rt += c;
-  }
-  return rt;
+ll get(ll x,int k){
+  return k*((x)/(k-1))+(x%(k-1))+1;
 }
 
-// ABC=>x
-string decode(string s,char x){
-  auto n=s.size();
-  string rt="";
-  
-  for(int i=0;i<n;++i){
-    if(i+2<n&&s[i]=='A'&&s[i+1]=='B'&&s[i+2]=='C'){
-      rt+=x;
-      i+=2;
-    }else rt+=s[i];
+ll make(int n,int k){
+  ll x=0;
+  rep(_,n-1){
+    x=get(x,k);
   }
-  
-  return rt;
-}
-
-bool check(const string& s){
-  int cnt[3]{0};
-  for(auto c:s) cnt[c-'A']++;
-
-  return (cnt[0]>0&&cnt[1]>0&&cnt[2]>0);
-}
-
-bool solve(){
-  string S;
-  cin >> S;
-
-  while(1){
-    if(!check(S))break;
-    if(S=="ABC") return true;
-
-    char item='k';
-    for(auto c='A';c<='C';c++){
-      if(S==encode(decode(S,c),c)){
-        item=c;
-      }
-    }
-    if(item=='k') break;
-    S=decode(S,item);
-  }
-  return false;
+  return x;
 }
 
 int main(){
-  cout << (solve()?"Yes":"No") << endl;
+  int n,k;
+  cin >> n>> k;
+  out(make(n,k));
 }
