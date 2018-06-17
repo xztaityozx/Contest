df158ec789a2745ea68620ede5e26ee06f734a6a
xztaityozx <taityonohibi@gmail.com>
2018-06-17 14:48:42 +0900

    AOJ 2021 お姫様の危機

--- cpp/contest.cpp
+++ cpp/contest.cpp
@@ -12,72 +12,79 @@ using ll=long long;
 using ull=unsigned long long;
 using vi=vector<int>;
 using vvc=vector<vector<char>>;
+using ti3=tuple<int,int,int>;
 template<typename T> void removeAt(vector<T>& v, int index) { v.erase(v.begin() + index); }
 
 
-bool isRect(const vvc& sq,int l,int r,int t,int b,char c){
-  for(int y=t;y<=b;++y)for(int x=l;x<=r;++x){
-    if(sq[y][x]!='-'&&sq[y][x]!=c) return false;
+ll solve(int n,int m,int l,int k,int a,int h){
+  vector<bool> coldlist(n,false);
+  rep(i,l) {
+    int li;
+    cin >> li;
+    coldlist[li]=true;
   }
-  return true;
-}
+  //coldlist[h]=true;
+  //coldlist[a]=true;
 
-void trim(vvc& sq,int l,int r,int t,int b){
-  for(int y=t;y<=b;++y)for(int x=l;x<=r;++x) sq[y][x]='-';
-}
-
-bool isClear(vvc& sq,int h,int w){
-  rep(y,h)rep(x,w) if(sq[y][x]!='.'&&sq[y][x]!='-') return false;
-  return true;
-}
-
-bool solve3(vvc& sq,int h,int w,char c){
-  int t=h,b=0,l=w,r=0;
-  rep(y,h) rep(x,w) if(sq[y][x]==c){
-    t=min(t,y);
-    b=max(b,y);
-    l=min(l,x);
-    r=max(r,x);
+  vector<pii> adj[n];
+  rep(i,k){
+    int a,b,c;
+    cin >> a>>b>>c;
+    adj[a].push_back(pii(b,c));
+    adj[b].push_back(pii(a,c));
   }
 
+  priority_queue<ti3,vector<ti3>,greater<ti3>> pq;
+  pq.push(make_tuple(a,m,0));
 
-  if(isRect(sq,l,r,t,b,c)){
-    trim(sq,l,r,t,b);
-    return true;
-  }
-  return false;
-}
+  ll dp[101][101]; //dp[i][j]=i番目の街にj分残して到達できる最短時間
+  rep(i,101)rep(j,101) dp[i][j]=1e9L;
+  dp[a][m]=0;
 
-bool solve2(vvc& sq,int h,int w){
-  map<char,bool> hash;
-  hash['-']=true;
-  hash['.']=true;
-  for(char c='A';c<='Z';c++) hash[c]=false;
-  rep(y,h)rep(x,w) {
-    if(!hash[sq[y][x]]){
-      if(solve3(sq,h,w,sq[y][x])) return true;
+  while(pq.size()!=0){
+    int to,cost,limit;
+    tie(to,limit,cost) = pq.top();pq.pop();
+    if(dp[to][limit]<cost) continue;
+
+    for(auto e :adj[to]){
+      int ct,cc;
+      tie(ct,cc)=e;
+      if(coldlist[to]){
+        //現在地点が回復ポイント
+        //m分になるまでt分休む
+        rep(t,m+1) if(limit+t<=m){
+          auto nl=limit+t-cc;
+          if(nl<0) continue;
+
+          auto nc=cc+t+cost;
+          if(nc<dp[ct][nl]){
+            dp[ct][nl]=nc;
+            pq.push(make_tuple(ct,nl,nc));
+          }
+        }
+      }else{
+        //回復ポイントでない
+        auto nl=limit-cc;
+        if(nl<0) continue;
+        auto nc=cost+cc;
+        if(nc<dp[ct][nl]){
+          dp[ct][nl]=nc;
+          pq.push(make_tuple(ct,nl,nc));
+        }
+      }
     }
-    hash[sq[y][x]]=true;
   }
-  return false;
-}
-
-bool solve(){
-  int h,w;
-  cin >> h>>w;
-  vvc box(h,vector<char>(w));
-  rep(y,h)rep(x,w){
-    cin >> box[y][x];
-  }
-
-  while(solve2(box,h,w));
-  
-
-  return isClear(box,h,w);
+  ll mm=1e9L;
+  rep(i,m) mm=min(mm,dp[h][i]);
+  return mm;
 }
 
 int main(){
-  int n;
-  cin >> n;
-  rep(i,n) if(solve()) cout << "SAFE" << endl; else cout << "SUSPICIOUS" << endl;
+  int N,M,L,K,A,H;
+  while(cin >> N >> M >> L>>K>>A>>H,(N|M|L|K|A|H)){
+    auto res=solve(N,M,L,K,A,H);
+    if(res==1e9L) cout << "Help!" << endl;
+    else cout << res << endl;
+  }
+  
 }
