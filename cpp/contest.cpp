8de849818bb3b1e43d5c45288ade7587466d1dae
xztaityozx <taityonohibi@gmail.com>
2018-06-19 00:48:53 +0900

    AOJ 2709 Dark Room

--- cpp/contest.cpp
+++ cpp/contest.cpp
@@ -16,20 +16,42 @@ using vvc=vector<vector<char>>;
 using ti3=tuple<int,int,int>;
 template<typename T> void removeAt(vector<T>& v, int index) { v.erase(v.begin() + index); }
 
-ll get(ll x,int k){
-  return k*((x)/(k-1))+(x%(k-1))+1;
-}
+int N,M,K,D[16];
+bool used[1<<16];
+int G[101][101];
 
-ll make(int n,int k){
-  ll x=0;
-  rep(_,n-1){
-    x=get(x,k);
+int solve(){
+  cin >> N>>M>>K;
+  rep(i,1<<16) used[i]=false;
+  rep(i,M) cin>>D[i],D[i]--;
+  rep(i,N)rep(j,K){
+    int v;
+    cin >> v;
+    v--;
+    G[i][j]=-1;
+    rep(k,M) if(i==D[k]) rep(l,M) if(v==D[l]) G[i][j]=l;
   }
-  return x;
+
+  queue<pii> Q;
+  Q.push(pii((1<<M)-1,0));
+
+  while(!Q.empty()){
+    int s,distance;
+    tie(s,distance)=Q.front();Q.pop();
+
+    if(used[s]) continue;
+    used[s]=true;
+    if(s==0) return distance;
+
+    rep(i,K){
+      int t=0;
+      rep(j,M) if(((s>>j)&1) && G[D[j]][i] >= 0) t|=1<<G[D[j]][i];
+      Q.push(pii(t,distance+1));
+    }
+  }
+  return 0;
 }
 
 int main(){
-  int n,k;
-  cin >> n>> k;
-  out(make(n,k));
+  out(solve());
 }
