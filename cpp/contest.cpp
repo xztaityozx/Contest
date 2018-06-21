a64f9b9a5bd198daac88aa22573efa2f6755ab11
xztaityozx <taityonohibi@gmail.com>
2018-06-21 23:46:56 +0900

    AOJ 2199 Differential Pulse Code Modulation

--- cpp/contest.cpp
+++ cpp/contest.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #define out(S) cout<<(S)<<endl;
 #define ShowAll(collection) for(auto i:collection){out(i);}
 #define beginend(v) v.begin(),v.end()
+#define Foreach(item,collection) for(auto item : collection)
 
 using pii=pair<int,int>;
 using vb=vector<bool>;
@@ -18,32 +19,12 @@ using ti3=tuple<int,int,int>;
 template<typename T> void removeAt(vector<T>& v, int index) { v.erase(v.begin() + index); }
 #define OCB(c) (c&1)|(c&2)<<2|(c&4)<<4|(c&8)<<6|(c&16)<<8|(c&32)<<10|(c&64)<<12|(c&128)<<14
 
-//dp[i]=iで表せるステージをクリアしたときの最小の攻略時間
-int dp[1<<16];
-
-int solve(int n){
-  vvi t(n,vi(n+1,0));
-  rep(i,n) rep(j,n+1) cin>>t[i][j];
-  rep(i,(1<<n)) dp[i]=1e9;
-
-  dp[0]=0;
-
-  rep(bit,1<<n){
-    rep(j,n) if(!((bit>>j)&1)) {
-      int ns=bit|(1<<j);
-      int nc=t[j][0];
-      rep(k,n) if((bit>>k)&1) nc=min(nc,t[j][k+1]);
-      nc+=dp[bit];
-      dp[ns]=min(nc,dp[ns]);
-    }
-  }
-  
-  return dp[(1<<n)-1];
-}
 
 int main(){
-  int n;
-  while(cin>>n,n){
-    out(solve(n));
-  }
+  auto f=[](int X)->int{
+    if(X<0) return 0;
+    if(X>255) return 255;
+    return X;
+  };
+  for(int x=-200;x<=300;++x) out(f(x));
 }
